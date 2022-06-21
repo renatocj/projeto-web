382217b425aac121d70f5a4748f7fd800f669e82
Renato Cacildo Junior <renato.cacildo@sga.pucminas.br>
2022-06-20 23:51:36 -0300

    add input pesquisar

--- assets/js/cadveic.js
+++ assets/js/cadveic.js
@@ -1,38 +1,38 @@
 function addVeic() {
-    const placa         = $('input[id=placa]').val();
-    const ano           = $('input[id=ano]').val();
-    const marca         = $('input[id=marca]').val();
-    const cor           = $('input[id=cor]').val();
-    const quilometragem = $('input[id=quilometragem]').val();
-    const cilindrada    = $('input[id=cilindrada]').val();
+  const placa = $('input[id=placa]').val();
+  const ano = $('input[id=ano]').val();
+  const marca = $('input[id=marca]').val();
+  const cor = $('input[id=cor]').val();
+  const quilometragem = $('input[id=quilometragem]').val();
+  const cilindrada = $('input[id=cilindrada]').val();
 
-    const dataObj = { placa, ano, marca, cor, quilometragem, cilindrada };
+  const dataObj = { placa, ano, marca, cor, quilometragem, cilindrada };
 
-    if (localStorage.getItem('veiculos') === null) {
-      
-      localStorage.setItem('veiculos', JSON.stringify([dataObj]));
+  if (localStorage.getItem('veiculos') === null) {
 
-    } else {
-      
-      localStorage.setItem('veiculos', JSON.stringify([...JSON.parse(localStorage.getItem('veiculos')),dataObj]));
+    localStorage.setItem('veiculos', JSON.stringify([dataObj]));
 
-    }
-    
-    limpaInput();
-    alert('Cadastro efetuado com sucesso!');
-    
+  } else {
+
+    localStorage.setItem('veiculos', JSON.stringify([...JSON.parse(localStorage.getItem('veiculos')), dataObj]));
+
   }
 
-    function limpaInput(){
-    placa         = $('input[id=placa]').val("");
-    ano           = $('input[id=ano]').val("");
-    marca         = $('input[id=marca]').val("");
-    cor           = $('input[id=cor]').val("");
-    quilometragem = $('input[id=quilometragem]').val("");
-    cilindrada    = $('input[id=cilindrada]').val("");
-    }
-    
-  function renderItem(veiculo) {
+  limpaInput();
+  alert('Cadastro efetuado com sucesso!');
+
+}
+
+function limpaInput() {
+  placa = $('input[id=placa]').val("");
+  ano = $('input[id=ano]').val("");
+  marca = $('input[id=marca]').val("");
+  cor = $('input[id=cor]').val("");
+  quilometragem = $('input[id=quilometragem]').val("");
+  cilindrada = $('input[id=cilindrada]').val("");
+}
+
+function renderItem(veiculo) {
   $('.veiculos').append(`
   <tr class="list-veiculos-item">
     <td>${veiculo.placa}</td>   
@@ -53,26 +53,54 @@ function addVeic() {
   tr>`);
 }
 
-  function getItems() {
-    const veiculos = JSON.parse(localStorage.getItem('veiculos'));
-    document.getElementById('tabela_veiculos').innerHTML = ''; 
-    veiculos.forEach(veiculo => renderItem(veiculo));
-  }
-  
-  function listar() {
-    getItems();
-  }
+function getItems() {
+  const veiculos = JSON.parse(localStorage.getItem('veiculos'));
+  document.getElementById('tabela_veiculos').innerHTML = '';
+  veiculos.forEach(veiculo => renderItem(veiculo));
+}
 
-  function deletar() {
-    localStorage.removeItem($veiculo.placa);
+function listar() {
+  getItems();
+}
 
-  }
+function deletar() {
+ // localStorage.removeItem($veiculo.placa);
 
-  function editar() {
+}
 
-    alert ('editando');
-  }
+function editar() {
 
-  function pesquisar() {
-    alert('estou pesquisando...');
-  }
+  alert('editando');
+}
+
+
+function limpaBusca() {
+
+  valorInput = $('input[id=pesquisa]').val("");
+
+}
+
+
+function pesquisar() {
+
+  const valor = $('input[id=pesquisa]').val();
+
+  const veiculos = JSON.parse(localStorage.getItem('veiculos'));
+
+  for (let i = 0; i < veiculos.length; i++) {
+
+    if (valor === veiculos[i].placa) {
+
+     // console.log(veiculos[i]);
+      renderItem(veiculos[i]);
+      limpaBusca();
+
+    } else {
+
+      console.log ("Placa incorreta. Tente novamente !!");
+      limpaBusca();
+
+    }
+  } 
+}
+
